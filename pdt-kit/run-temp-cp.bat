cef22bf79bee01d35eea7662f620e66504a8bd2e
stephentetley <stephen.tetley@gmail.com>
2022-08-18 22:30:41 +0100

    Initial work on a replacement PDT parser library.

---
+++ pdt-kit/run-temp-cp.bat
@@ -0,0 +1,12 @@
+
+java -jar ..\..\..\flix\bin\flix-working.jar build-pkg
+
+java -cp "./lib/*;../../../flix/bin/flix-working.jar" ca.uwaterloo.flix.Main ./temp/run-read.flix ^
+    ./pdt-kit.fpkg ^
+    ./lib/basis-base.fpkg ^
+    ./lib/flix-time.fpkg ^
+    ./lib/interop-base.fpkg ^
+    ./lib/monad-lib.fpkg ^
+    ./lib/sheetio.fpkg ^
+    ./lib/withindex-classes.fpkg
+
