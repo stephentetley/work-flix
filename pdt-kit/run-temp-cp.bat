6cbd135ada2c7da069ee1b06a5c5766f0af9be97
stephentetley <stephen.tetley@gmail.com>
2022-08-24 23:05:39 +0100

    PDTKit - initial work towards outputting the low level AST as JSON.

--- pdt-kit/run-temp-cp.bat
+++ pdt-kit/run-temp-cp.bat
@@ -3,6 +3,7 @@ java -jar ..\..\..\flix\bin\flix-working.jar build-pkg
 
 java -cp "./lib/*;../../../flix/bin/flix-working.jar" ca.uwaterloo.flix.Main ./temp/run-read.flix ^
     ./pdt-kit.fpkg ^
+    ./lib/interop-json.fpkg ^
     ./lib/basis-base.fpkg ^
     ./lib/flix-time.fpkg ^
     ./lib/interop-base.fpkg ^
