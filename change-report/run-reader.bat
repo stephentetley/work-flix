b0037b69ec549f9a8eefb32af1af266f2e11e0ba
stephentetley <stephen.tetley@gmail.com>
2022-09-26 22:09:04 +0100

    Work towards writing changes to SQLite. Currently has stack overflow...

--- change-report/run-reader.bat
+++ change-report/run-reader.bat
@@ -3,6 +3,7 @@ java -jar ..\..\..\flix\bin\flix-working.jar build-pkg
 
 java -cp "./lib/*;../../../flix/bin/flix-working.jar" ca.uwaterloo.flix.Main ./temp/run-reader.flix ^
     ./change-report.fpkg ^
+    ./lib/flix-basicdb.fpkg ^
     ./lib/basis-json.fpkg ^
     ./lib/interop-json.fpkg ^
     ./lib/basis-base.fpkg ^
